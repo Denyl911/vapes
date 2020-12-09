4ada6f13b0403c181a12188596481e2f374c00c7
Denilson <Denyl@mx>
2020-12-08 22:12:21 -0600

    dist files folder fixed

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import express from 'express'
 import hisory from 'connect-history-api-fallback'
+import path from 'path'
 
 let port = process.env.PORT || 3000
 
 const app = express()
 
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static(path.resolve(__dirname, '..',  'dist')))
 
-app.use(hisory({ index: '/dist/index.html'}))
+app.use(hisory({ index: path.resolve(__dirname + '..',  'dist', 'index.html')}))
 
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static(path.resolve(__dirname, '..', 'dist')))
 
 app.listen(port, () => console.log(`App listening on port ${port}`))
